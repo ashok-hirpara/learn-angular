408f1087f4e5d517f22773e52272d7f13b1b7dfb
Ashok Hirpara <ashokhirpara00768@gmail.com>
2020-12-30 15:48:29 +0530

    running the app built manually

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `<h1>Core Angular</h1>`,
+  styles: [`
+    h1 {
+      background-color: #FFF;
+      text-align: center;
+    }
+  `],
+})
+export class AppComponent {}
